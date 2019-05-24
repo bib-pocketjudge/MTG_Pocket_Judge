24bb0b83423182c4651ed33408017bbe716d8787
Adrian Ledwinka / PBS2H17A <pbs2h17ale@m22.pb.bib.de>
2019-05-24 11:13:34 +0200

    Initial commit

---
+++ MTG_Pocket_Judge/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MTG_Pocket_Judge
+//
+//  Created by Adrian Ledwinka / PBS2H17A on 5/24/19.
+//  Copyright © 2019 Adrian Ledwinka / PBS2H17A. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ MTG_Pocket_JudgeTests/MTG_Pocket_JudgeTests.swift
@@ -0,0 +1,34 @@
+//
+//  MTG_Pocket_JudgeTests.swift
+//  MTG_Pocket_JudgeTests
+//
+//  Created by Adrian Ledwinka / PBS2H17A on 5/24/19.
+//  Copyright © 2019 Adrian Ledwinka / PBS2H17A. All rights reserved.
+//
+
+import XCTest
+@testable import MTG_Pocket_Judge
+
+class MTG_Pocket_JudgeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
