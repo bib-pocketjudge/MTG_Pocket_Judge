a562280c80ca2b334d9764c8cf052794ded4dfa2
pbs2h17ale <adrian.ledwinka@edu.bib.de>
2019-05-24 13:07:07 +0200

    Getting JSON blob + parsing JSON

--- MTG_Pocket_Judge/MTG_Pocket_Judge/ViewController.swift
+++ MTG_Pocket_Judge/MTG_Pocket_Judge/ViewController.swift
@@ -13,6 +13,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let jsonTestUrl = "https://api.scryfall.com/cards/named?exact=doomsday"
+        
+        guard let url = URL(string: jsonTestUrl) else { return }
+        
+        URLSession.shared.dataTask(with: url)
+        { (data,response, err) in
+            
+            guard let data = data else { return }
+            
+//            let dataAsString = String(data: data, encoding: .utf8)
+            
+ //           print(dataAsString)
+            
+            do
+            {
+                let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+                //let doomsday = Card(json)
+            }
+            catch let jsonErr
+            {
+                print("Error serializing json:",jsonErr)
+            }
+            
+        }.resume()
     }
 
     //Commit git
